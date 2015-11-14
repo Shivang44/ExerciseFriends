dffa4cc0d57f36fb227ca50ad99d92a14089f669
Shivang Saxena <shivangs44@gmail.com>
2015-11-14 16:28:07 -0500

    Made login page

--- ExerciseFriends.js
+++ ExerciseFriends.js
@@ -3,7 +3,7 @@ if (Meteor.isClient) {
   Session.setDefault('counter', 0);
 
   Template.body.helpers({
-    loggedin: false
+
     // Through javascript, set this to true if logged in
   });
 
@@ -12,6 +12,40 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
+  });
+
+  Template.main.helpers({
+      emails: function(){
+          return Meteor.user().emails[0].address;
+      }
+  });
+
+
+  Template.splash.events({
+      'submit #login-form' : function(e, t){
+          e.preventDefault();
+
+          // retrive values from form
+
+          var email = t.find('#inputEmail').value;
+          var password = t.find('#inputPassword').value;
+
+          /* Use for registration
+          var allow_signup = true;
+          if(!email.endsWith("osu.edu")){
+                alert('You must be an OSU student to use this app!');
+                allow_signup = false;
+          }
+          */
+
+          Meteor.loginWithPassword(email, password, function(err){
+              if (err) {
+                  alert('Incorrect email/password');
+              }
+          });
+
+          return false;
+      }
   });
 }
 
